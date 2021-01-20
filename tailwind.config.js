4609c396f0b621d68399931740cc0f8cbf13d2b0
Peppek1993 <peppek1993@gmail.com>
2021-01-20 22:45:49 +0100

    Netlify deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: ['./src/**/*.vue'],
+  purge: {
+    content: ['./src/**/*.vue'],
+    options: {
+      safelist: ['text-pink-200', 'text-purple-200', 'text-red-200']
+    }
+  },
   theme: {},
   variants: {},
   plugins: []
