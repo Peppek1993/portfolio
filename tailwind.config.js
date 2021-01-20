a58d942b836f4098124f4ef86408bc008d3abf1a
Peppek1993 <peppek1993@gmail.com>
2021-01-20 21:43:51 +0100

    Netlify deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      backgroundColor: {
+        primary: '#3490dc'
+      }
+    }
   },
   variants: {
     extend: {}
