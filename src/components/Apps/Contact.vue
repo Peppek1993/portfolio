ca2331a77754f8a15c4b169080aea07a2dca92c7
Peppek1993 <peppek1993@gmail.com>
2021-01-21 00:26:13 +0100

    Contact form setup

--- src/components/Apps/Contact.vue
+++ src/components/Apps/Contact.vue
@@ -1,37 +1,48 @@
 <template>
   <section class="text-gray-700 body-font p-4">
     <div>
-      <div class="flex flex-col w-full rounded-lg">
+      <form
+        action="POST"
+        data-netlify="true"
+        class="flex flex-col w-full rounded-lg"
+      >
         <div>
           <input
+            id="email"
             type="email"
             name="email"
             placeholder="email"
             class="w-full px-4 py-2 mb-4 mr-4 bg-gray-100 rounded-lg"
+            required
           />
         </div>
         <div>
           <input
+            id="name"
             type="name"
             name="name"
             placeholder="name"
             class="w-full px-4 py-2 mb-4 mr-4 bg-gray-100 rounded-lg"
+            required
           />
         </div>
         <div class=" ">
           <textarea
+            id="message"
             type="message"
             name="message"
             placeholder="message"
             class="w-full px-4 h-40 py-2 mb-4 mr-4 bg-gray-100 rounded-lg"
+            required
           ></textarea>
         </div>
         <button
+          type="submit"
           class="px-8 py-2 font-semibold text-white bg-blue-500 rounded-lg shadow-xl"
         >
-          Button
+          Send message
         </button>
-      </div>
+      </form>
     </div>
   </section>
 </template>
@@ -40,4 +51,8 @@
 export default {}
 </script>
 
-<style></style>
+<style>
+textarea {
+  resize: none;
+}
+</style>
