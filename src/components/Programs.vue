fe054015e00ef3709cc7c87c9eb86199f270601a
Peppek1993 <peppek1993@gmail.com>
2021-01-20 22:36:31 +0100

    Netlify deploy

--- src/components/Programs.vue
+++ src/components/Programs.vue
@@ -6,9 +6,15 @@
       :key="index"
       class="pb-24 textShadow"
     >
-      <vue-draggable-resizable handles="[]" :resizable="false" :w="80" :h="80">
+      <vue-draggable-resizable
+        :handles="[]"
+        :resizable="false"
+        :w="80"
+        :h="80"
+        class-name="icon"
+      >
         <div
-          class="appClass h-20 w-20 border-none border"
+          class="appClass h-20 w-20 border-none"
           @click="
             openApp(app)
             getClickPosition($event, index)
