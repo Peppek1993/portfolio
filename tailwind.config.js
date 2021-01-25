7da1114f380a88938ebca2ce816bf596be14eed2
Peppek1993 <peppek1993@gmail.com>
2021-01-25 19:21:27 +0100

    Tailwindcss Purge fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: {
     content: ['./src/**/*.vue'],
     options: {
-      safelist: ['text-pink-200', 'text-purple-200', 'text-red-200']
+      safelist: [
+        'text-pink-200',
+        'text-purple-200',
+        'text-red-200',
+        'text-green-300',
+        'text-pink-300',
+        'text-yellow-300',
+        'text-indigo-300',
+        'text-red-300'
+      ]
     }
   },
   theme: {},
